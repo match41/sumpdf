eab8b5f1b8b275e98e75141636ee1266bb3dfbb9
Matchman <match41@users.sourceforge.net>
2010-03-26 18:56:36 +0800

    trying to improve error handling on fonts

--- libpdfdoc/src/font/Win32FontDb.cpp
+++ libpdfdoc/src/font/Win32FontDb.cpp
@@ -17,7 +17,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 \***************************************************************************/
 
-/**	\file	Win32FontDb.cc
+/**	\file	Win32FontDb.cpp
 	\brief	implementation of the Win32FontDb class
 	\date	Feb 14, 2010
 	\author	Nestal Wan
@@ -106,7 +106,8 @@ std::vector<unsigned char> Win32FontDb::FindFont(
 		GetFontData( hdc, 0, 0, &result[0], size ) ;
 		return result ;
 	}
-	return std::vector<unsigned char>( ) ;	
+	
+	throw GetLastError( ) ;
 }
 
 } // end of namespace
