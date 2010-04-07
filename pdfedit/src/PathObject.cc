a33efb68fff988c10cfb60ee3cc430e83a436c58
Matchman <match41@users.sourceforge.net>
2010-04-08 00:06:15 +0800

    refactored path code to new class

--- pdfedit/src/PathObject.cc
+++ pdfedit/src/PathObject.cc
@@ -17,78 +17,52 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 \***************************************************************************/
 
-/**	\file	PageLoader.cc
-	\brief	implementation of the PageLoader class
-	\date	Mar 26, 2010
+/**	\file	PathObject.cc
+	\brief	implementation of the PathObject class
+	\date	Apr 7, 2010
 	\author	Nestal Wan
 */
 
-#include "PageLoader.hh"
-
-// local headers
-#include "GlyphGroup.hh"
 #include "PathObject.hh"
 
 // libpdfdoc headers
 #include <graphics/Path.hh>
 #include <graphics/PathSegment.hh>
-#include <graphics/Text.hh>
-#include <util/Debug.hh>
 
-// Qt headers
-//#include <QGraphicsPathItem>
-#include <QGraphicsScene>
-#include <QPainterPath>
-
-// boost headers
-#include <boost/bind.hpp>
+#include <QPainter>
 
 namespace pdf {
 
 /**	constructor
 	
 */
-PageLoader::PageLoader( QGraphicsScene *scene )
-	: m_scene( scene )
+PathObject::PathObject( const Path *path )
+	: m_path( QPointF(0, 0) )
 {
-	PDF_ASSERT( m_scene != 0 ) ;
-}
-
-void PageLoader::VisitText( Text *text )
-{
-	PDF_ASSERT( text != 0 ) ;
-
-	std::for_each( text->begin(), text->end(),
-		boost::bind( &PageLoader::LoadTextLine, this, _1 ) ) ;
-}
-
-void PageLoader::LoadTextLine( const TextLine& line )
-{
-	PDF_ASSERT( m_scene != 0 ) ;
-	m_scene->addItem( new GlyphGroup( line ) ) ;
-}
-
-void PageLoader::VisitGraphics( Graphics *gfx )
-{
-}
-
-void PageLoader::VisitPath( Path *path )
-{
-/*	QPainterPath qppath ;
 	for ( std::size_t i = 0 ; i < path->Count() ; ++i )
 	{
 		PathSegment seg = path->Segment(i) ;
 		switch ( seg.GetOp() )
 		{
-			case PathSegment::move : qppath.moveTo( seg[0], seg[1] ) ; break ;
-			case PathSegment::line : qppath.lineTo( seg[0], seg[1] ) ; break ;
-			case PathSegment::close: qppath.closeSubpath( ) ; break ;
+			case PathSegment::move : m_path.moveTo( seg[0], seg[1] ) ; break ;
+			case PathSegment::line : m_path.lineTo( seg[0], seg[1] ) ; break ;
+			case PathSegment::close: m_path.closeSubpath( ) ; break ;
 			default : break ;
 		}
 	}
-	QGraphicsPathItem *pi = new QGraphicsPathItem( qppath ) ;*/
-	PathObject *pi = new PathObject( path ) ;
-	m_scene->addItem( pi ) ;
+}
+
+QRectF PathObject::boundingRect( ) const
+{
+	return m_path.boundingRect() ;
+}
+
+void PathObject::paint(
+	QPainter 						*painter,
+	const QStyleOptionGraphicsItem	*option,
+	QWidget 						*widget ) 
+{
+	painter->drawPath( m_path ) ;
 }
 
 } // end of namespace
---
+++ pdfedit/src/PathObject.hh
@@ -0,0 +1,59 @@
+/***************************************************************************\
+ *   Copyright (C) 2006 by Nestal Wan                                      *
+ *   me@nestal.net                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2.                              *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+\***************************************************************************/
+
+/**	\file	PathObject.hh
+    \brief	definition the PathObject class
+    \date	Apr 7, 2010
+    \author	Nestal Wan
+*/
+
+#ifndef __PDF_PATHOBJECT_HH_EADER_INCLUDED__
+#define __PDF_PATHOBJECT_HH_EADER_INCLUDED__
+
+#include <QAbstractGraphicsShapeItem>
+
+#include <QPainterPath>
+
+namespace pdf {
+
+class Path ;
+
+///	brief description
+/**	\internal
+	The PathObject class represents
+*/
+class PathObject : public QAbstractGraphicsShapeItem
+{
+public :
+	PathObject( const Path *path ) ;
+
+	// virtual functions for QGraphicsItem
+	QRectF boundingRect( ) const ;
+	void paint(
+		QPainter 						*painter,
+		const QStyleOptionGraphicsItem	*option,
+		QWidget 						*widget ) ; 
+
+private :
+	QPainterPath	m_path ;
+} ;
+
+} // end of namespace
+
+#endif // PATHOBJECT_HH_
